2f0ef15ba87ca5cb3ddcc3f5f83393edcccbc3b3
Sagar Acharya <sagar_acharya@live.in>
2016-09-04 00:14:06 +0530

    Scope, & assign default values

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+	after_initialize :set_defaults
+
+	scope :almost_completed, -> { where('percent_complete > 75.0') }
+	scope :not_completed, -> { where('percent_complete < 75.0') }
+
+	def set_defaults
+		self.percent_complete ||= 0.0
+	end
 end
